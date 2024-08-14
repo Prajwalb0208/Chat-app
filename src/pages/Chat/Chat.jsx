b1f89829775d022d98921fcefda570928e6119a5
Prajwal B <92373236+Prajwalb0208@users.noreply.github.com>
2024-08-14 23:24:11 +0530

    Basic file structure And Login Page UI

---
+++ src/pages/Chat/Chat.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import './Chat.css'
+const Chat = () => {
+  return (
+    <div>Chat Page</div>
+  )
+}
+
+export default Chat
---
+++ src/pages/ProfileUpdate/ProfileUpdate.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import './ProfileUpdate.css'
+const ProfileUpdate = () => {
+  return (
+    <div>ProfileUpdate Page</div>
+  )
+}
+
+export default ProfileUpdate
