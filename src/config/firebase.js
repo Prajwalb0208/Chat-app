1cd8b3b60011e6a27faaa6aee200ef984bb67820
Prajwal B <92373236+Prajwalb0208@users.noreply.github.com>
2024-08-17 21:39:36 +0530

    Firebase COnnection and Signin Feature

---
+++ src/config/firebase.js
@@ -0,0 +1,43 @@
+
+import { initializeApp } from "firebase/app";
+import { createUserWithEmailAndPassword, getAuth } from "firebase/auth";
+import { getFirestore, setDoc ,doc } from "firebase/firestore";
+import { toast } from "react-toastify";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBhnzEBGDfa-4z116cM5Tv89zC0KiXbvdw",
+  authDomain: "chat-app-gs-38220.firebaseapp.com",
+  projectId: "chat-app-gs-38220",
+  storageBucket: "chat-app-gs-38220.appspot.com",
+  messagingSenderId: "660384285364",
+  appId: "1:660384285364:web:cfc027f532689fed4c8de1"
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getFirestore(app);
+
+const signup=async (username,email,password) => {
+    try {
+        const res = await createUserWithEmailAndPassword(auth,email,password);
+        const user = res.user;
+        await setDoc(doc(db,"users",user.uid),{
+            id:user.uid,
+            username:username.toLowerCase(),
+            email,
+            name:"",
+            avatar:"",
+            bio:"Hey, There I am using ChatApp",
+            lastSeen:Date.now()
+        })
+        await setDoc(doc(db,"chats",user.uid),{
+            chatData:[]
+        })
+        // 2 Collections user details and chatdata
+    } catch (error) {
+        console.error(error)
+        toast.error(error.code)
+    }
+}
+
+export {signup}
